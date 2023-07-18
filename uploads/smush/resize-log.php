fee52afc86cfe1fe4c7f2e7a0923ebc9a409a683
natchaw <bam@brightdolphins.com>
2023-07-18 10:33:56 +0700

    initial commit

---
+++ uploads/smush/resize-log.php
@@ -0,0 +1,14 @@
+<?php die(); ?>
+[2023-06-23T08:42:31+00:00] Setting animated meta value
+[2023-06-23T08:42:31+00:00] Info: Background wp_smush_background_scan_process: Process completed.
+[2023-06-23T08:42:44+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"1376"}.
+[2023-06-23T08:42:54+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"1813"}.
+[2023-06-23T08:43:02+00:00] Warning: Background wp_smush_bulk_smush_background_process: Time/Memory limits reached, save the queue and dispatch a new request.
+[2023-06-23T08:43:09+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"1926"}.
+[2023-06-23T08:43:13+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"2208"}.
+[2023-06-26T02:36:25+00:00] Setting animated meta value
+[2023-06-26T02:38:05+00:00] Setting animated meta value
+[2023-06-26T02:52:31+00:00] Info: Background wp_smush_background_scan_process: Process completed.
+[2023-06-26T02:57:35+00:00] Setting animated meta value
+[2023-06-26T02:58:11+00:00] Setting animated meta value
+[2023-06-26T04:08:05+00:00] Setting animated meta value
---
+++ uploads/smush/smush-log.php
@@ -0,0 +1,49 @@
+<?php die(); ?>
+[2023-02-03T10:20:04+00:00] Notice: Loopback check successful.
+[2023-02-03T10:20:04+00:00] Info: Background wp_smush_bulk_smush_background_process: Starting new process with 6 tasks
+[2023-02-03T10:20:04+00:00] Info: Background wp_smush_bulk_smush_background_process: Spawning a brand new instance (ID: 43f5e78488bf0c7d8e999c2b549d940b) for the process.
+[2023-02-03T10:20:04+00:00] Info: Background wp_smush_bulk_smush_background_process: Dispatching a new request for instance 43f5e78488bf0c7d8e999c2b549d940b.
+[2023-02-03T10:20:04+00:00] Info: Background wp_smush_bulk_smush_background_process: Scheduling new event with hook wp_smush_bulk_smush_background_process_cron to run wp_smush_bulk_smush_background_process_cron_interval.
+[2023-02-03T10:20:05+00:00] Info: Background wp_smush_bulk_smush_background_process: Handling instance ID 43f5e78488bf0c7d8e999c2b549d940b.
+[2023-02-03T10:20:05+00:00] Info: Background wp_smush_bulk_smush_background_process: Setting last run timestamp for instance ID 43f5e78488bf0c7d8e999c2b549d940b to 2023-02-03 10:20:05
+[2023-02-03T10:20:05+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"1064"}.
+[2023-02-03T10:20:05+00:00] Error: Error encountered while smushing attachment ID 1064:Skipped (6 MB), file size limit of 5mb exceeded
+[2023-02-03T10:20:05+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"1376"}.
+[2023-02-03T10:20:05+00:00] Error: Error encountered while smushing attachment ID 1376:Skipped (5 MB), file size limit of 5mb exceeded
+[2023-02-03T10:20:05+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"1813"}.
+[2023-02-03T10:20:05+00:00] Error: Error encountered while smushing attachment ID 1813:Skipped (8 MB), file size limit of 5mb exceeded
+[2023-02-03T10:20:05+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"1896"}.
+[2023-02-03T10:20:05+00:00] Error: Error encountered while smushing attachment ID 1896:Skipped (5 MB), file size limit of 5mb exceeded
+[2023-02-03T10:20:05+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"1926"}.
+[2023-02-03T10:20:05+00:00] Error: Error encountered while smushing attachment ID 1926:Skipped (5 MB), file size limit of 5mb exceeded
+[2023-02-03T10:20:05+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"2208"}.
+[2023-02-03T10:20:05+00:00] Error: Error encountered while smushing attachment ID 2208:Skipped (5 MB), file size limit of 5mb exceeded
+[2023-02-03T10:20:05+00:00] Info: Background wp_smush_bulk_smush_background_process: Process completed.
+[2023-02-03T10:20:05+00:00] Info: Background wp_smush_bulk_smush_background_process: Cancelling event with hook wp_smush_bulk_smush_background_process_cron.
+[2023-02-03T10:20:05+00:00] Info: Bulk Smush completed for http://localhost:8888/BD-Test, and reporting email is disabled.
+[2023-06-23T08:42:31+00:00] Info: Background wp_smush_background_scan_process: Starting new process with 1 tasks
+[2023-06-23T08:42:31+00:00] Info: Background wp_smush_background_scan_process: Spawning a brand new instance (ID: 3317dd9c38aba165d728e5b47bfe36e3) for the process.
+[2023-06-23T08:42:31+00:00] Info: Background wp_smush_background_scan_process: Dispatching a new request for instance 3317dd9c38aba165d728e5b47bfe36e3.
+[2023-06-23T08:42:31+00:00] Info: Background wp_smush_background_scan_process: Scheduling new event with hook wp_smush_background_scan_process_cron to run wp_smush_background_scan_process_cron_interval.
+[2023-06-23T08:42:31+00:00] Info: Background wp_smush_background_scan_process: Setting last run timestamp for instance ID 3317dd9c38aba165d728e5b47bfe36e3 to 2023-06-23 08:42:31
+[2023-06-23T08:42:31+00:00] Info: Background wp_smush_background_scan_process: Executing task 1.
+[2023-06-23T08:42:31+00:00] Info: Background wp_smush_background_scan_process: Cancelling event with hook wp_smush_background_scan_process_cron.
+[2023-06-23T08:42:39+00:00] Info: Background wp_smush_bulk_smush_background_process: Starting new process with 6 tasks
+[2023-06-23T08:42:39+00:00] Info: Background wp_smush_bulk_smush_background_process: Spawning a brand new instance (ID: f40bacee60a6df39bd266f8c033b6cbe) for the process.
+[2023-06-23T08:42:39+00:00] Info: Background wp_smush_bulk_smush_background_process: Dispatching a new request for instance f40bacee60a6df39bd266f8c033b6cbe.
+[2023-06-23T08:42:39+00:00] Info: Background wp_smush_bulk_smush_background_process: Scheduling new event with hook wp_smush_bulk_smush_background_process_cron to run wp_smush_bulk_smush_background_process_cron_interval.
+[2023-06-23T08:42:39+00:00] Info: Background wp_smush_bulk_smush_background_process: Setting last run timestamp for instance ID f40bacee60a6df39bd266f8c033b6cbe to 2023-06-23 08:42:39
+[2023-06-23T08:42:39+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"1064"}.
+[2023-06-23T08:43:02+00:00] Info: Background wp_smush_bulk_smush_background_process: Dispatching a new request for instance f40bacee60a6df39bd266f8c033b6cbe.
+[2023-06-23T08:43:03+00:00] Info: Background wp_smush_bulk_smush_background_process: Setting last run timestamp for instance ID f40bacee60a6df39bd266f8c033b6cbe to 2023-06-23 08:43:03
+[2023-06-23T08:43:03+00:00] Info: Background wp_smush_bulk_smush_background_process: Executing task {"type":"SMUSH","image_id":"1896"}.
+[2023-06-23T08:43:13+00:00] Info: Background wp_smush_bulk_smush_background_process: Process completed.
+[2023-06-23T08:43:13+00:00] Info: Background wp_smush_bulk_smush_background_process: Cancelling event with hook wp_smush_bulk_smush_background_process_cron.
+[2023-06-23T08:43:13+00:00] Info: Bulk Smush completed for http://localhost:8888/BD-Test, and reporting email is disabled.
+[2023-06-26T02:52:30+00:00] Info: Background wp_smush_background_scan_process: Starting new process with 1 tasks
+[2023-06-26T02:52:30+00:00] Info: Background wp_smush_background_scan_process: Spawning a brand new instance (ID: 34d9813f92c6d7e8b1cd1d45d3750b50) for the process.
+[2023-06-26T02:52:30+00:00] Info: Background wp_smush_background_scan_process: Dispatching a new request for instance 34d9813f92c6d7e8b1cd1d45d3750b50.
+[2023-06-26T02:52:30+00:00] Info: Background wp_smush_background_scan_process: Scheduling new event with hook wp_smush_background_scan_process_cron to run wp_smush_background_scan_process_cron_interval.
+[2023-06-26T02:52:31+00:00] Info: Background wp_smush_background_scan_process: Setting last run timestamp for instance ID 34d9813f92c6d7e8b1cd1d45d3750b50 to 2023-06-26 02:52:31
+[2023-06-26T02:52:31+00:00] Info: Background wp_smush_background_scan_process: Executing task 1.
+[2023-06-26T02:52:31+00:00] Info: Background wp_smush_background_scan_process: Cancelling event with hook wp_smush_background_scan_process_cron.

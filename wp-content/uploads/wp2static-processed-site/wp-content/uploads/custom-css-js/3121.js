3a7249bfd8b49b8306e6deb10b66269f56224abd
NamChatchalida <nam@brightdolphins.com>
2023-11-09 16:52:58 +0700

    Revert "Junior React private"
    This reverts commit 79fb55419e0054d4ac7e50ab2c677afb6876b2a7.

---
+++ wp-content/uploads/wp2static-processed-site/wp-content/uploads/custom-css-js/3121.js
@@ -0,0 +1,18 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+function yourprefix_remove_title_youtube_oembed( $html, $url, $args ) {
+
+    if( strpos( $url, 'youtu.be' ) !== false || strpos( $url, 'youtube.com' ) !== false ) :
+
+        return str_replace( '?feature=oembed', '?feature=oembed&amp;showinfo=0&amp;rel=0', $html );
+
+    else:
+
+        return $html;
+
+    endif;
+
+}
+add_filter( 'embed_oembed_html', 'yourprefix_remove_title_youtube_oembed', 10, 3 );
+</script>
+<!-- end Simple Custom CSS and JS -->
